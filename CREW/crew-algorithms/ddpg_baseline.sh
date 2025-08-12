d2cec4b3dd2eb2e2c61b8bfdd8d6a268df5c4269
jzr01 <zhengran.ji@duke.edu>
2025-08-12 12:03:20 -0400

    save work

--- CREW/crew-algorithms/ddpg_baseline.sh
+++ CREW/crew-algorithms/ddpg_baseline.sh
@@ -3,8 +3,3 @@ source ./utils.sh
 
 run_ddpg_baseline1 "42 43 44 45 46" "hide_and_seek_1v1" "True"
 run_ddpg_baseline1 "42 43 44 45 46" "hide_and_seek_1v1" "False"
-
-source ./utils.sh
-
-run_ddpg_baseline "1 2 7 10 13 15 17 18 25 30 32 33 37 39 44" "hide_and_seek_1v1" "True"
-run_ddpg_baseline "1 2 7 10 13 15 17 18 25 30 32 33 37 39 44" "hide_and_seek_1v1" "False"
--- CREW/crew-algorithms/ddpg_eval_local.sh
+++
@@ -1,6 +0,0 @@
-export PYTHONPATH=:$PYTHONPATH
-source ./utils.sh
-
-eval_ddpg_local "1 2 7 10 13 15 17 18 25 30 32 33 37 39 44" "find_treasure" "vote_ref"
-eval_ddpg_local "1 2 7 10 13 15 17 18 25 30 32 33 37 39 44" "hide_and_seek_1v1" "vote_ref"
-
